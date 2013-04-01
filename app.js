f3de01f626be9858294a086003ea397a929a8cf8
Matt Jadud <matt@jadud.com>
2013-03-31 21:51:42 -0400

    Shipping code, sending a response.

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
+// base64.encode, base64.decode
+require('./libs/base64.js')
+
+
 var express = require('express');
 var app = express();
 
-var top = function (req, res) {
-  var body = 'The Top';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
-}
-
-app.get('/', top);
-
-app.get('/hello.txt', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use('/ide', express.static(__dirname + '/ide'));  
 });
 
-app.use('/ide', express.static(__dirname + '/ide'));
+var runCompiler = function (req, resp) {
+  // var code = req.body.code; 
+  data = req.body;
+  console.log(data);
+  console.log(base64.decode(data.code));
+  
+  data = {};
+  data.result = "SUCCESS";
+  resp.json(data);
+}
+
+app.post('/compile', runCompiler);
 
 var THEPORT = process.env.PORT || 3000
 app.listen(THEPORT);

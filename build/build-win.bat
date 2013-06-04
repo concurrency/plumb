c30bfee25dacface9772afefa20a09a520913130
Matt Jadud <matt@jadud.com>
2013-06-04 18:58:57 -0400

    Tweaking windows build.

--- build/build-win.bat
+++ build/build-win.bat
@@ -2,21 +2,22 @@
 
 echo. Setting up variables
 
-set src=C:\Documents and Settings\Administrator\My Documents\GitHub\flow
+set src=../
 set build=%src%\build
-set ddname=Flow
-set dest=%src%\build\%ddname%
+set ddname=Plumb
+set dest=%src%\build-win\%ddname%
 set raco="C:\Program Files\Racket\raco.exe"
 set zip="C:\Program Files\7-Zip\7z.exe"
 
 REM REMOVE THE DESTINATION IF IT EXISTS
-cd "%build%"
-rm -rf %ddname%
+cd "%src%"
+rm -rf "build-win"
 REM REMOVE THE ZIPFILE TOO
 rm -rf %ddname%.zip
 
 REM MAKE DESTINATION DIRECTORY
 echo. Making destination directories.
+mkdir "build-win"
 mkdir "%dest%"
 
 REM MAKE TEMP DIRECTORY IN DESTINATION
@@ -25,17 +26,15 @@ mkdir "%dest%\temp"
 REM BUILD EXECUTABLE
 echo. Building executable.
 cd %build%
-%raco% exe -o %ddname%.exe "%src%\server\gui.rkt"
+%raco% exe -o %ddname%.exe "%src%\plumb-gui.rkt"
 
 REM MAKE IT DISTRIBUTABLE
 echo. Making executable distributable.
 %raco% distribute "%dest%" %ddname%.exe
 
 REM COPY NEEDED DIRECTORIES
-call:xcopy_sd bin
-call:xcopy_sd interface
-call:xcopy_sd occam
-call:xcopy_sd config
+cd %src%
+call:xcopy_sd client-config 
 
 REM ZIP EVERYTHING
 echo. Zipping things up.
@@ -46,7 +45,8 @@ echo. See URL noted in script.
 REM http://stackoverflow.com/questions/3669001/getting-ssh-agent-to-work-with-git-run-from-windows-command-shell
 
 echo. Doing SCP.
-scp -i id_rsa %ddname%.zip jadudm@transterpreter.org:/srv/www/org/transterpreter.download/files/flow/
+REM scp -i id_rsa %ddname%.zip jadudm@transterpreter.org:/srv/www/org/transterpreter.download/files/flow/
+
 
 
 REM End of script

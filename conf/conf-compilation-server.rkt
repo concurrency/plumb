07c05660332be6746cf793e6d3f4d9f21c07b22c
Matt Jadud <matt@jadud.com>
2013-06-05 07:46:46 -0400

    Renaming URL for testing. They are repositories...

--- conf/conf-compilation-server.rkt
+++ conf/conf-compilation-server.rkt
@@ -1,2 +1,4 @@
 #hash((host . "ec2-54-226-131-120.compute-1.amazonaws.com")
-      (port . "9000"))
+      (port . "9000")
+      (examples . "https://raw.github.com/concurrency/plumbing-examples/master/repositories.conf")
+      )

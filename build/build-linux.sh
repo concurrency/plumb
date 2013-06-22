6a51558dcadda075248d69d841136eace68b87b8
Matt Jadud <matt@jadud.com>
2013-06-22 13:12:36 -0400

    Make sure we use the right icon.

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -1,44 +1,66 @@
-BASE=Flow
-DEST=../${BASE}
-SRC=../server
-rm -rf ${DEST}
-mkdir -p ${DEST}
+#!/bin/bash
 
-pushd ${SRC}
-  echo Building the Flow executable.
-  raco exe --gui -o ${BASE} gui.rkt
-  echo Sleeping
-  sleep 2
-  echo Readying for Distribution
-  raco distribute ${DEST} ${BASE}
-  echo Sleeping
-  sleep 2
-  echo Setting executable mode.
-  chmod 755 ${DEST}/bin/${BASE}
+echo Setting Up Variables
+
+DATE=`date +%Y%m%d`
+SRC=~/git/plumb
+BUILD=$SRC/build-linux
+DDNAME=Plumb
+DEST=$BUILD/$DDNAME-$DATE
+RACO=raco
+ZIP=tar
+PSCP=scp
+SOURCE=ide.rkt
+ICON=omer-icon
+
+pushd "$SRC"
+  rm -rf "$BUILD"
 popd
- 
-echo Moving files around
-sleep 2
-cp -R ../bin/unix/ ${DEST}/bin/unix/
-cp -R ../config/ ${DEST}/config/
-cp -R ../interface/ ${DEST}/interface/
-cp -R ../occam/ ${DEST}/occam/
-mkdir -p ${DEST}/temp
-# Hopefully eliminating the "not found" errors
-# that happen... for reasons unknown.
-mkdir -p ${DEST}/conf
-touch ${DEST}/conf/not-found.html
 
-echo Going up one level
-pushd ..
-  echo Zipping.
-  tar cvzf Flow.tgz Flow/
+echo   Making Destination Directories
 
-  echo If the first command line argument was "upload"...
-  if [ "$1" = "upload" ]; then
-        echo Uploading
-	scp -i ~/.ssh/jadudm-rsa ${BASE}.tgz jadudm@transterpreter.org:/srv/www/org/transterpreter.download/files/flow/
+pushd "$SRC"
+  mkdir "$BUILD"
+popd
+
+pushd "$BUILD"
+  mkdir "$DEST"
+popd
+
+pushd "$BUILD"
+  echo   Build Executable
+  cp "$SRC"/build/${ICON}.ico "$BUILD"
+  "$RACO" exe --ico ${ICON}.ico -o "$DDNAME" "$SRC/$SOURCE"
+  rm ${ICON}.ico
+  echo   Make It Distributable
+  "$RACO" distribute "$DEST" "$DDNAME"
+  echo Remove the original build to eliminate confusion
+  rm "$BUILD/$DDNAME"
+popd
+
+echo Copy Needed Directories
+cp -R "$SRC/client-config" "$DEST/client-config"
+
+pushd "$BUILD"
+  echo Zip Everything
+  "$ZIP" cvzf "$DDNAME-$DATE.tar.gz" "$DDNAME-$DATE"
+  if [[ $1 = "upload" ]]; then
+    if [[ -f ~/.ssh/small-imac-berea ]]; then
+      KEY=~/.ssh/small-imac/berea
+    fi
+    if [[ -f ~/.ssh/big-mac-berea ]]; then
+      KEY=~/.ssh/big-mac-berea
+    fi
+
+    scp -i "$KEY" "$DDNAME-$DATE".tar.gz jadudm@jadud.com:~/jadud.com/downloads/   
+    echo http://jadud.com/downloads/$DDNAME-$DATE.tar.gz
   fi
 popd
 
 
+# echo SCP Everything
+# "$PSCP" "$DDNAME.zip" jadudm@transterpreter.org:/srv/www/org/transterpreter.download/files/flow/
+
+
+
+echo End Of Script
